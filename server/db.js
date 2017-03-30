8016fe0569b22ffa8df37eafe9fc14dbf4168143
Scott <smclendening@gmail.com>
2017-03-29 21:53:47 -0700

    Create Postgres queries to create tables

--- server/db.js
+++ server/db.js
@@ -1,9 +1,21 @@
 var pg = require('pg');
-var knex = require('../test/db/knex.js').config;
+// var knex = require('../test/db/knex.js').config;
 
-var client = new pg.Client(knex.connection);
-console.log('database: ', knex.connection);
+// var client = new pg.Client(knex.connection);
+// console.log('database: ', knex.connection);
 
-client.connect();
+// client.connect();
 
-module.exports = client;
+// module.exports = client;
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
