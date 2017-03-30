af5ed31266ff2aa8ce8e18a897763541f79fbd4c
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-30 16:55:45 -0700

    (feat) Setup db connection to elephantSQL

--- server/db.js
+++ server/db.js
@@ -6,27 +6,28 @@ var models = require('./db/Models.js');
 // client.connect();
 // module.exports = client;
 
-pg.defaults.ssl = true;
-var client = pg.connect(process.env.GGDB_URL, function(err, client) {
+pg.defaults.ssl = false;
+var client = new pg.Client('AddLinkHere');
+client.connect(function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-  client('DROP TABLE session, users, categories, comments, followers, investments, pitches, votes');
-  client
-    .query(models.sessionTable);
-  client
-    .query(models.usersTable);
-  client
-    .query(models.categoriesTable);
-  client
-    .query(models.commentsTable);
-  client
-    .query(models.followersTable);
-  client
-    .query(models.investmentsTable);
-  client
-    .query(models.pitchesTable);
-  client
-    .query(models.votesTable);
+  // client.query('DROP TABLE session, users, categories, comments, followers, investments, pitches, votes');
+  // client
+  //   .query(models.sessionTable);
+  // client
+  //   .query(models.usersTable);
+  // client
+  //   .query(models.categoriesTable);
+  // client
+  //   .query(models.commentsTable);
+  // client
+  //   .query(models.followersTable);
+  // client
+  //   .query(models.investmentsTable);
+  // client
+  //   .query(models.pitchesTable);
+  // client
+  //   .query(models.votesTable);
 });
 
 
