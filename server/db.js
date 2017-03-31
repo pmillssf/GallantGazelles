ea629bf79b63f81b3208fc6714faa28c82c3e56a
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-30 19:42:10 -0700

    (test) Troubleshooting heroku postgress

--- server/db.js
+++ server/db.js
@@ -7,28 +7,19 @@ var models = require('./db/Models.js');
 // module.exports = client;
 
 pg.defaults.ssl = true;
-var client = pg.connect(process.env.GGDB_URL, function(err, client) {
+pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-  client('DROP TABLE session, users, categories, comments, followers, investments, pitches, votes');
-  client
-    .query(models.sessionTable);
-  client
-    .query(models.usersTable);
-  client
-    .query(models.categoriesTable);
-  client
-    .query(models.commentsTable);
-  client
-    .query(models.followersTable);
-  client
-    .query(models.investmentsTable);
-  client
-    .query(models.pitchesTable);
-  client
-    .query(models.votesTable);
+  client.('DROP TABLE session, users, categories, comments, followers, investments, pitches, votes');
+  client.query(models.sessionTable);
+  client.query(models.usersTable);
+  client.query(models.categoriesTable);
+  client.query(models.commentsTable);
+  client.query(models.followersTable);
+  client.query(models.investmentsTable);
+  client.query(models.pitchesTable);
+  client.query(models.votesTable);
 });
-
 
 
 module.exports = client;
