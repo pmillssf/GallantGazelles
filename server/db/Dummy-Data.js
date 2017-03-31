7ada9fbcce8cbe389d14088e0070e9844087bf27
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-31 11:07:14 -0700

    (test) Comment out seed data functions, except votes
    Database loaded all other seed data properly, votes has been
    rewritten and will need to be commented out next pull request

--- server/db/Dummy-Data.js
+++ server/db/Dummy-Data.js
@@ -8,33 +8,33 @@ var comments = require('./Comments.js');
 
 
 module.exports.loadData = function() {
-users.createUser('User1', '123', 'Profile for User1');
-users.createUser('User2', '123', 'Profile for User2');
-users.createUser('User3', '123', 'Profile for User3');
+// users.createUser('User1', '123', 'Profile for User1');
+// users.createUser('User2', '123', 'Profile for User2');
+// users.createUser('User3', '123', 'Profile for User3');
 
 
-pitches.addPitch(1, 'Pitch 1', 'Pitch 1 Video URL', 'Pitch 1 Website URL', 'Pitch 1 Profile', 'Pitch 1 Blurb', '1');
-pitches.addPitch(2, 'Pitch 2', 'Pitch 2 Video URL', 'Pitch 2 Website URL', 'Pitch 2 Profile', 'Pitch 2 Blurb', '2');
+// pitches.addPitch(1, 'Pitch 1', 'Pitch 1 Video URL', 'Pitch 1 Website URL', 'Pitch 1 Profile', 'Pitch 1 Blurb', '1');
+// pitches.addPitch(2, 'Pitch 2', 'Pitch 2 Video URL', 'Pitch 2 Website URL', 'Pitch 2 Profile', 'Pitch 2 Blurb', '2');
 
-followers.postNewPitchFollower(1, 1);
-followers.postNewPitchFollower(1, 2);
-followers.postNewPitchFollower(2, 2);
-followers.postNewPitchFollower(2, 1);
+// followers.postNewPitchFollower(1, 1);
+// followers.postNewPitchFollower(1, 2);
+// followers.postNewPitchFollower(2, 2);
+// followers.postNewPitchFollower(2, 1);
 
-categories.addCategory('Tech');
-categories.addCategory('Games');
-categories.addCategory('Books');
-categories.addCategory('iPhone');
-categories.addCategory('Android');
-categories.addCategory('Productivity');
+// categories.addCategory('Tech');
+// categories.addCategory('Games');
+// categories.addCategory('Books');
+// categories.addCategory('iPhone');
+// categories.addCategory('Android');
+// categories.addCategory('Productivity');
 
 votes.voteOnPitch(1, 1, 1);
-votes.voteOnPitch(1, 2, -1);
-votes.voteOnPitch(2, 1, -1);
-votes.voteOnPitch(2, 2, 0);
+votes.voteOnPitch(-1, 2, 1);
+votes.voteOnPitch(-1, 1, 2);
+votes.voteOnPitch(0, 2, 2);
 
-comments.createCommentInComments( 1, 1, 'Hello');
-comments.createCommentInComments( 2, 1, 'Yo');
-comments.createCommentInComments( 3, 1, 'What');
-comments.createCommentInComments( 1, 1, 'No');
+// comments.createCommentInComments( 1, 1, 'Hello');
+// comments.createCommentInComments( 2, 1, 'Yo');
+// comments.createCommentInComments( 3, 1, 'What');
+// comments.createCommentInComments( 1, 1, 'No');
 }
