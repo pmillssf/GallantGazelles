564de8631df3c6359b790db4db3b3cd03b993586
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-30 23:50:51 -0700

    (fix) Comment out table creation, tables were inserted via postico

--- server/db.js
+++ server/db.js
@@ -11,15 +11,15 @@ var client = new pg.Client(process.env.DATABASE_URL);
 client.connect(function (err) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-  client.query('DROP TABLE IF IT EXISTS session, users, categories, comments, followers, investments, pitches, votes');
-  client.query(models.sessionTable);
-  client.query(models.usersTable);
-  client.query(models.categoriesTable);
-  client.query(models.commentsTable);
-  client.query(models.followersTable);
-  client.query(models.investmentsTable);
-  client.query(models.pitchesTable);
-  client.query(models.votesTable);
+  // client.query('DROP TABLE IF EXISTS session, users, categories, comments, followers, investments, pitches, votes');
+  // client.query(models.sessionTable);
+  // client.query(models.usersTable);
+  // client.query(models.categoriesTable);
+  // client.query(models.commentsTable);
+  // client.query(models.followersTable);
+  // client.query(models.investmentsTable);
+  // client.query(models.pitchesTable);
+  // client.query(models.votesTable);
 });
 
 console.log('client', client);
